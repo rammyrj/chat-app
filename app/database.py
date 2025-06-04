d0636b6ad7d6c698a1ce2a12b65e7738d0205206
rammyrj <rashidthegreat82@gmail.com>
2025-06-04 23:09:22 +0530

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = AsyncIOMotorClient(os.getenv("MONGO_URL"))
+db = client["chat_app"]
