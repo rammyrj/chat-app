7064db413e8be89adcb720c4582c90cdba5aefb4
rammyrj <rashidthegreat82@gmail.com>
2025-06-04 23:35:03 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,6 @@ from app import auth, chat
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(chat.router)
+@app.get("/")
+def root():
+    return {"message": "Backend is working!"}
